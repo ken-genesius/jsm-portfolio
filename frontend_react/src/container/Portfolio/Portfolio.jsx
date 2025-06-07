aa510854c4fcf461d9ba932a7befb1a850ec2f03
ken-genesius <mkengenesius@gmail.com>
2025-06-08 02:10:28 +0700

    feat: finished the portfolio

--- frontend_react/src/container/Portfolio/Portfolio.jsx
+++ frontend_react/src/container/Portfolio/Portfolio.jsx
@@ -1,33 +1,119 @@
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import React, { useRef } from "react";
 import { AppWrap, MotionWrap } from "../../wrapper";
+import StackIcon from "tech-stack-icons";
+import { MdStars } from "react-icons/md";
 
 import "./portfolio.scss";
 
 const items = [
   {
     id: 1,
+    bgColor: "#84563f",
     title: "React Commerce",
     img: "https://images.pexels.com/photos/30872819/pexels-photo-30872819/free-photo-of-elegant-coffee-set-on-a-sunlit-table-in-ankara.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+    points: [
+      "Built a financial calculator to assist customers in determining credit card options.",
+      "Developed a user-friendly interface with Laravel and Tailwind.",
+      "Optimized the back-end logic for accuracy and efficiency.",
+    ],
+    url: "#",
+    stacks: [
+      {
+        title: "HTML5",
+        icon: "html5",
+      },
+      {
+        title: "JavaScript",
+        icon: "js",
+      },
+      {
+        title: "Bootstrap",
+        icon: "bootstrap4",
+      },
+    ],
   },
   {
     id: 2,
+    bgColor: "#8ed2e9",
     title: "Next.js Blog",
     img: "https://images.pexels.com/photos/30600496/pexels-photo-30600496/free-photo-of-vibrant-in-n-out-burger-exterior-in-los-angeles.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+    url: "#",
+    points: [
+      "Built a financial calculator to assist customers in determining credit card options.",
+      "Developed a user-friendly interface with Laravel and Tailwind.",
+      "Optimized the back-end logic for accuracy and efficiency.",
+    ],
+    stacks: [
+      {
+        title: "HTML5",
+        icon: "html5",
+      },
+      {
+        title: "JavaScript",
+        icon: "js",
+      },
+      {
+        title: "Bootstrap",
+        icon: "bootstrap4",
+      },
+    ],
   },
   {
     id: 3,
+    bgColor: "#c3ada0",
     title: "Vanilla JS App",
     img: "https://images.pexels.com/photos/30891641/pexels-photo-30891641/free-photo-of-luxury-desert-tents-in-omani-dunes.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+    url: "#",
+    points: [
+      "Built a financial calculator to assist customers in determining credit card options.",
+      "Developed a user-friendly interface with Laravel and Tailwind.",
+      "Optimized the back-end logic for accuracy and efficiency.",
+    ],
+    stacks: [
+      {
+        title: "HTML5",
+        icon: "html5",
+      },
+      {
+        title: "JavaScript",
+        icon: "js",
+      },
+      {
+        title: "Bootstrap",
+        icon: "bootstrap4",
+      },
+    ],
   },
   {
     id: 4,
+    bgColor: "#b96c44",
     title: "Music App",
     img: "https://images.pexels.com/photos/31236777/pexels-photo-31236777/free-photo-of-traditional-chinese-architecture-with-modern-buildings.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+    url: "#",
+    points: [
+      "Built a financial calculator to assist customers in determining credit card options.",
+      "Developed a user-friendly interface with Laravel and Tailwind.",
+      "Optimized the back-end logic for accuracy and efficiency.",
+    ],
+    stacks: [
+      {
+        title: "HTML5",
+        icon: "html5",
+      },
+      {
+        title: "JavaScript",
+        icon: "js",
+      },
+      {
+        title: "Bootstrap",
+        icon: "bootstrap4",
+      },
+    ],
   },
 ];
 
@@ -45,13 +131,63 @@ const Single = ({ item }) => {
     <section>
       <div className="app__portfolio-container">
         <div className="app__portfolio-wrapper">
-          <div className="app__portfolio-imageContainer" ref={ref}>
-            <img src={item.img} alt="" />
+          <div className="app__portfolio-section" ref={ref}>
+            <div className="app__portfolio-section-portfolio">
+              <div
+                className="app__portfolio-section-content"
+                style={{ backgroundColor: item.bgColor }}
+              >
+                <motion.a
+                  href={item.url}
+                  className="app__portfolio-section-link"
+                  whileInView={{ scale: 1 }}
+                  whileHover={{ scale: 1.05 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <span>{item.title}</span>
+                  <span>{">"}</span>
+                </motion.a>
+                <motion.div
+                  className="app__portfolio-section-image"
+                  whileInView={{ y: 0 }}
+                  whileHover={{ y: "-20%" }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <img src={item.img} alt="" />
+                </motion.div>
+              </div>
+            </div>
           </div>
           <motion.div className="app__portfolio-textContainer" style={{ y }}>
             <h2>{item.title}</h2>
+            <div className="app__portfolio-techstack-container">
+              {item.stacks.map((tStack, index) => (
+                <div
+                  className="app__portfolio-techstack"
+                  key={item.id + "-" + tStack.title + "-" + index}
+                >
+                  <StackIcon
+                    className="app__portfolio-techstack-icon"
+                    name={tStack.icon}
+                  />
+                  <span className="app__portfolio-techstack-title">
+                    {tStack.title}
+                  </span>
+                </div>
+              ))}
+            </div>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <div className="app__portfolio-point-container">
+              {item.points.map((point, index) => (
+                <div
+                  className="app__portfolio-point"
+                  key={item.id + "-points-" + index}
+                >
+                  <MdStars className="app__portfolio-point-icon" />
+                  <span className="app__portfolio-point-span">{point}</span>
+                </div>
+              ))}
+            </div>
           </motion.div>
         </div>
       </div>
@@ -60,6 +196,24 @@ const Single = ({ item }) => {
 };
 
 const Portfolio = () => {
+  const textVariant = (delay) => {
+    return {
+      hidden: {
+        y: -50,
+        opacity: 0,
+      },
+      show: {
+        y: 0,
+        opacity: 1,
+        transition: {
+          type: "spring",
+          duration: 1.25,
+          delay: delay,
+        },
+      },
+    };
+  };
+
   const ref = useRef();
 
   const { scrollYProgress } = useScroll({
@@ -73,11 +227,19 @@ const Portfolio = () => {
   });
 
   return (
-    <div className="app__portfolio-work" ref={ref}>
-      {items.map((item) => (
-        <Single item={item} key={item.id} />
-      ))}
-    </div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className="p-text">What I have made</p>
+        <h2 className="head-text">
+          Projects & <span>Portfolios</span>
+        </h2>
+      </motion.div>
+      <div className="app__portfolio-work" ref={ref}>
+        {items.map((item) => (
+          <Single item={item} key={item.id} />
+        ))}
+      </div>
+    </>
   );
 };
 
