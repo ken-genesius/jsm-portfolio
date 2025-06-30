8715c6c759da5c6f25f48260729309c57afbd2f9
ken-genesius <mkengenesius@gmail.com>
2025-06-30 13:48:14 +0700

    fix: responsive for phones

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -18,11 +18,11 @@ const App = () => {
       <Navbar />
       <Header />
       <About />
-      <Work />
+      {/* <Work /> */}
       <Portfolio />
       <Skills />
       <Experience />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
     </div>
   );
